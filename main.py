dfa0e63b3a52c7473b7d0754e584f5b1cb60edf9
SloppySlime <114940023+2Sloppy4slime@users.noreply.github.com>
2025-05-11 22:20:27 +0200

    drop la mise a jour
    oui

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pyxel
 import random
 
 pyxel.init(160, 120, title = "INVESTCLICKR",fps = 30)
+pyxel.mouse(True)
 
 oneSecTimer_val = 30
 def oneSecTimer():
@@ -42,8 +43,32 @@ goal = random.randrange(500,1500)
 time_s = 0
 time_m = 0
 
+mousex = 0
+mousey = 0 
 maxrollpricechange = 300
 minrollpricechange = 0
+
+
+
+def buy() :
+    global money
+    global stock_price
+    global stocks
+    global bought
+    if money > stock_price :
+            stocks += 1
+            money -= stock_price
+            bought += 1
+def sell() :
+    global stocks
+    global money
+    global stock_price
+    global sold
+    if stocks > 0:
+            money += stock_price
+            stocks -= 1 
+            sold += 1
+
 
 def update():
     global money
@@ -54,6 +79,8 @@ def update():
     global stock_history
     global maxrollpricechange
     global minrollpricechange
+    global mousex
+    global mousey
     if pyxel.btnp(pyxel.KEY_ESCAPE):
         pyxel.quit()
     
@@ -61,13 +88,23 @@ def update():
         print("you win")
         pyxel.quit()
 
+    if pyxel.btnp(pyxel.MOUSE_BUTTON_LEFT):
+        mousey = pyxel.mouse_x
+        mousex = pyxel.mouse_y
+        print(str(mousex) + " ; " + str(mousey))
+        if mousey > 116 and mousey < 158 and mousex > 104 and mousex < 114:
+            sell()
+        if mousey > 116 and mousey < 158 and mousex > 93 and mousex < 103:
+            buy()
+
     if oneSecTimer()  :
         deflation = 0
         inflation = 0
         if bought >0:
-            deflation = (random.randrange(0 - bought*40, 100) * 0.01)
+            inflation = (random.randrange(0 , 0 +  bought*40) * 0.01)
         if sold >0:
-            inflation = (random.randrange(0 , 0 +  sold*40) * 0.01)
+            deflation = (random.randrange(0 - sold*40, 100) * 0.01)
+            
 
         stock_price = round(stock_price + (float(random.randrange( minrollpricechange , maxrollpricechange)-150) * 0.01) + deflation + inflation , 2)
         #eviter la merde où ça se bloque ds les coins
@@ -94,17 +131,10 @@ def update():
 
 
     if pyxel.btnp(pyxel.KEY_C): #acheter
-        if money > stock_price :
-            stocks += 1
-            money -= stock_price
-            bought += 1
-            
-            #stock_price *= (random.randrange(70,100) * 0.01)
+        buy()
+
     if pyxel.btnp(pyxel.KEY_N): #vendre
-        if stocks > 0:
-            money += stock_price
-            stocks -= 1 
-            sold += 1
+        sell()
 
 def draw():
     global stock_history
@@ -125,13 +155,18 @@ def draw():
     # 5 + (80 - stock_price * 16 ) si 1<stock_price<5
     # (80 - 11*stock_price) si 0<stock_price<1
 
+    # boutons
+    pyxel.rect(116,104,38,10,3) #(116,104) -> (154,114)
+    pyxel.rect(116,93,38,10,3) #(116,93) -> (154,103)
+    pyxel.text(116,106,"   sell",7)
+    pyxel.text(115,95,"    buy",7)
+
 
     pyxel.text(10,85,"money: " + str(money),7)
     pyxel.text(10,95,"stocks: " + str(stocks),7)
     pyxel.text(10,105,"price: " + str(stock_price),7)
-    pyxel.text(115,5,"raise " + str(goal) ,7)
-    pyxel.text(115,105," n: sell",7)
-    pyxel.text(115,95," c: buy",7)
+    pyxel.text(115,5," make " + str(goal) ,7)
+    
     pyxel.text(125,85,str(time_m) + " : " + str(time_s),7)
 
 
